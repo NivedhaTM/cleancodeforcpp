8472a3464fbbdb34e40392a4c389775408838174
NivedhaTM <68527473+NivedhaTM@users.noreply.github.com>
2020-07-21 23:18:20 +0530

    changing access modifiers

--- paramchecker.h
+++ paramchecker.h
@@ -2,11 +2,11 @@
 class VitalData{
   private:
     float bpm, spo2, respRate ;
-
-  public:
-    VitalData(float bpm, float spo2, float respRate);
     bool isBpmOk() ;
     bool isSpo2Ok();
     bool isRespRateOk() ;
+
+  public:
+    VitalData(float bpm, float spo2, float respRate);
     bool isVitalsOk() ;
 };
