6afc8e75a30eecf7eb251a44bffff0df3c4709bb
NivedhaTM <68527473+NivedhaTM@users.noreply.github.com>
2020-07-21 22:58:39 +0530

    updating contr defn

--- paramchecker.h
+++ paramchecker.h
@@ -4,12 +4,7 @@ class VitalData{
     float bpm, spo2, respRate ;
 
   public:
-    void setBpm(float bpmValue) ;
-    float getBpm() ;
-    void setSpo2(float spo2Value) ;
-    float getSpo2() ;
-    void setRespRate(float respRateValue) ;
-    float getRespRate() ;
+    VitalData(float bpm, float spo2, float respRate);
     bool isBpmOk() ;
     bool isSpo2Ok();
     bool isRespRateOk() ;
