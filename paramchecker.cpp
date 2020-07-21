321332e91a087c29d8f6e9e3334e892c2ad8870a
NivedhaTM <68527473+NivedhaTM@users.noreply.github.com>
2020-07-21 16:41:45 +0530

    Updated condition/logic checks

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -21,23 +21,19 @@ class VitalData{
     float getRespRate() {
       return respRate;
     }
-
-  bool isBpmOk() {
-    return ( bpm < 70 || bpm > 150);
-  }
-
-  bool isSpo2Ok() {
-  return  spo2 < 80;
-  }
-
-  bool isRespRateOk() {
-  return (respRate < 30 || respRate > 60);
-  }
+    bool isBpmOk() {
+      return !( bpm < 70 || bpm > 150);
+    }
+    bool isSpo2Ok() {
+      return  !spo2 < 80;
+    }
+    bool isRespRateOk() {
+      return !(respRate < 30 || respRate > 60);
+    }
   
-  bool isVitalsOk() {
-        return !( isBpmOk() || isSpo2Ok() || isRespRateOk());
-  }
-
+    bool isVitalsOk() {
+        return ( isBpmOk() && isSpo2Ok() && isRespRateOk());
+    }
 };
 
 
