cc1e9a0b7228f4ac615b79ecb6a36d0ee561c052
NivedhaTM <68527473+NivedhaTM@users.noreply.github.com>
2020-07-22 15:52:45 +0530

    adding derived class info

--- vitals.h
+++ vitals.h
@@ -5,3 +5,27 @@ class Vitals{
   public:
     bool isVitalOk() ;
 };
+
+class BPM{
+  protected:
+    float vitalValue ;
+
+  public:
+    bool isVitalOk() ;
+};
+
+class SPO2{
+  protected:
+    float vitalValue ;
+
+  public:
+    bool isVitalOk() ;
+};
+
+class RespRate{
+  protected:
+    float vitalValue ;
+
+  public:
+    bool isVitalOk() ;
+};
